08f6384c1fe02769f89c42c970a4ae3630cf3aae
Alex <jenjer444@gmail.com>
2024-03-23 20:53:38 +0200

    initial commit

---
+++ tictactoeweb/Hubs/TicTacHub.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace tictactoeweb.Hubs
+{
+    public class TicTacHub : Hub
+    {
+        public static int Id { get; set; } = 1;
+        public static int UsersOnline { get; set; } = 0;
+        public static int UsersAll { get; set; } = 0;
+        public static int MoveCounter { get; set; } = 0;
+        public async Task SetUserId()
+        {
+            await Clients.Caller.SendAsync("GetUserId", Id);
+            Id++;
+        }
+
+        public int passMovePlayerId { get; set; } = 1;
+
+        public async Task GetPassMove()
+        {
+            await Clients.All.SendAsync("PassMove", passMovePlayerId);
+        }
+
+        public async Task SendPlayerMove(int userId, int cell, int winIndex)
+        {
+            MoveCounter++;
+
+            if (winIndex != 0 || MoveCounter == 9)
+            {
+
+                if (MoveCounter == 9)
+                {
+                    winIndex = 3;
+                }
+
+                await GameOver(winIndex);
+            }
+            else
+            {
+                switchPassMoveId(userId);
+                await GetPassMove();
+            }
+
+            await Clients.All.SendAsync("ReceiveMove", userId, cell, winIndex);
+
+            
+
+        }
+
+        public async Task GameOver(int winIndex)
+        {
+            await GetGameStatus(winIndex);
+            await GetWinMessage(winIndex);
+        }
+
+        private void switchPassMoveId(int userId)
+        {
+            if (userId == 1)
+            {
+                passMovePlayerId = 2;
+            }
+            else if (userId == 2)
+            {
+                passMovePlayerId = 1;
+            }
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            UsersOnline--;
+
+            await Clients.All.SendAsync("GetOnlineUsers", UsersOnline, UsersAll);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task GetGameStatus(int winIndex)
+        {
+            await Clients.All.SendAsync("GameStatus", winIndex == 0);
+        }
+
+        public async Task GetWinMessage(int winIndex)
+        {
+            string winner = "";
+
+            switch (winIndex)
+            {
+                case 1:
+                    winner = "first player WON";
+                    break;
+                case 2:
+                    winner = "second player WON";
+                    break;
+                case 3:
+                    winner = "DRAW";
+                    break;
+            }
+
+            await Clients.All.SendAsync("WinMessage", winner);
+        }
+
+        public async Task GetCurrentPlayer()
+        {
+            await Clients.All.SendAsync("GetCurrentPlayer", passMovePlayerId);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            UsersOnline++;
+            UsersAll++;
+
+            await GetCurrentPlayer();
+
+            await Clients.All.SendAsync("GetOnlineUsers", UsersOnline, UsersAll);
+
+            await GetPassMove();
+
+            await SetUserId();
+
+            await base.OnConnectedAsync();
+        }
+
+    }
+}
---
+++ tictactoeweb/wwwroot/js/site.js
@@ -0,0 +1,75 @@
+﻿class TicTacToe {
+    constructor() {
+        this.playground = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+
+    playerId = 1;
+    moveCounter = 0;
+    winIndex = 0;
+
+    switchPlayer() {
+        if (this.playerId == 1) {
+            this.playerId = 2;
+        }
+        else {
+            this.playerId = 1;
+        }
+    }
+
+    checkWin() {
+        for (let i = 0; i < 9; i += 3) {
+            if (this.playground[i] === this.playerId &&
+                this.playground[i + 1] === this.playerId &&
+                this.playground[i + 2] === this.playerId) {
+                return true;
+            }
+        }
+
+        for (let i = 0; i < 3; i++) {
+            if (this.playground[i] === this.playerId &&
+                this.playground[i + 3] === this.playerId &&
+                this.playground[i + 6] === this.playerId) {
+                return true;
+            }
+        }
+
+        if ((this.playground[0] === this.playerId && this.playground[4] === this.playerId && this.playground[8] === this.playerId) ||
+            (this.playground[2] === this.playerId && this.playground[4] === this.playerId && this.playground[6] === this.playerId)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    moveCount() {
+        this.moveCounter++;
+
+        if (this.moveCounter == 9) {
+            if (this.winIndex == 0) {
+                this.winIndex = 3;
+            }
+        }
+    }
+
+    makeMove(cell, userId) {
+        if (this.playground[cell] === 0) {
+            this.playground[cell] = userId;
+
+            if (this.checkWin()) {
+                this.winIndex = userId;
+
+                console.log("Win by player: " + userId);
+                return true; // Выход, чтобы не сменять игрока после победы
+            }
+
+            this.switchPlayer();
+            this.moveCount();
+
+            return true;
+        } else {
+            console.log("incorrect cell");
+            return false;
+        }
+    }
+
+}
